b2bd81995c8436f59d3f047b4c6e09c126531793
joacojachow <joacojachow@hotmail.com>
2025-04-04 10:25:26 -0300

    carwo logo added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { useState } from "react"
@@ -35,8 +35,8 @@ export default function Navbar() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
-          <Link href="/" className="font-bold text-2xl">
-            CARWO
+          <Link href="/" className="font-bold text-2xl ml-10">
+            <Image src="/carwo_logo.svg" alt="logo" width={125} height={125} />
           </Link>
         </div>
 
